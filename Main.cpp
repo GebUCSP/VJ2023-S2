aa405a6a04c1490727d6e5aa173c625b0ae53fa0
José Gabriel Cornejo Castro <147185580+GebUCSP@users.noreply.github.com>
2023-10-06 19:12:30 -0500

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,14 @@
+#include "Platform/Platform.hpp"
+#include "new3.h"
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+	Game juego;
+	juego.run();
+
+	return 0;
+}
