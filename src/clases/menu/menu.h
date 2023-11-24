45dede1d80698f13a4f37841a5984af7a7222a3e
Laconchaloura <arnolsmith.tacca@ucsp.edu.pe>
2023-11-24 04:57:55 +0100

    Implementacion de los botones

--- src/clases/menu/menu.h
+++ src/clases/menu/menu.h
@@ -4,17 +4,20 @@
 #include <iostream>
 #include "SFML/Graphics.hpp"
 #include "SFML/Graphics/Texture.hpp"
-
+#include <SFML/Window/Mouse.hpp>
 
 class Menu{
     friend class Juego;
 private:
 
+    bool:: a_play= false,a_options= false;
+
     sf::RenderWindow* window;
     sf::Texture background;
     sf::Sprite backgroundImage;
 
-    sf::RectangleShape play;
+    sf::RectangleShape play,options;
+
 public:
     //Constructor
     Menu(sf::RenderWindow* _window): window{_window}{
@@ -27,10 +30,14 @@ public:
         sf::Vector2u sizeW = window->getSize();
         backgroundImage.setScale(static_cast<float> (sizeW.x) / background.getSize().x, static_cast<float> (sizeW.y) / background.getSize().y);
 
-        //boton
-        play.setPosition(static_cast<float>(sizeW.x)/4,static_cast<float>(sizeW.y)/3);
-        play.setSize(sf::Vector2f (500,100));
+        //boton play
+        play.setPosition(static_cast<float>(sizeW.x)-180,static_cast<double>(sizeW.y)-200);
+        play.setSize(sf::Vector2f (300,100));
         play.setFillColor(sf::Color::Transparent);
+        //boton options
+        options.setPosition(static_cast<float>(sizeW.x)-180,static_cast<double>(sizeW.y)-100);
+        options.setSize(sf::Vector2f (550,100));
+        options.setFillColor(sf::Color::Transparent);
     }
 
     void checkMouseClick(){
@@ -39,7 +46,14 @@ public:
 
         if (play.getGlobalBounds().contains(mousePosCoords)){
             if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-                std::cout << "se logro" << std::endl;
+                a_play= true;
+                std::cout << "Play presionado" << std::endl;
+            }
+        }
+        if (options.getGlobalBounds().contains(mousePosCoords)){
+            if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+                a_options= true;
+                std::cout << "Options presionado" << std::endl;
             }
         }
     }
